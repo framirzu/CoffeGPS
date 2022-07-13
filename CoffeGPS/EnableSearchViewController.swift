a27c9326201c42cf4b46c4288abdc1d4de89cbfd
Franzua <framirzu@emeal.nttdata.com>
2022-07-13 16:25:51 -0500

    Constrainst actualizados

--- CoffeGPS/EnableSearchViewController.swift
+++ CoffeGPS/EnableSearchViewController.swift
@@ -5,50 +5,6 @@
 ////  Created by Franzua Renzo Ramirez Gaston Zuloeta on 25/06/22.
 ////
 //
-//import UIKit
-//
-//class EnableSearchViewController: UIViewController {
-//
-//
-//    @IBOutlet var startToSearchButton: UIButton!
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        // Do any additional setup after loading the view.
-//    }
-//func startRequestLocation() {
-//
-//    LocationManager.shared.getLocation()
-//    if LocationManager.shared.seeAuthorizationStatus() > 0 {
-//
-//               startToSearchButton.isEnabled = true
-//
-//        }else {
-//                print("Ejecucion de cierre")
-//            startToSearchButton.isEnabled = false
-//        }
-//
-//    }
-//
-//@IBAction func didTapbutton(){
-//
-//            LocalRepository().getCafeterias { cafeterias in
-//
-//                let storyboard = UIStoryboard(name: "Main", bundle: nil)
-//                let vc = storyboard.instantiateViewController(withIdentifier: "ListCafeteriasViewController") as! ListCafeteriasViewController
-//
-//                //Llamamos a LocalRepository
-//                vc.cafeterias = cafeterias.sorted{ (a, b) -> Bool in
-//                    return a.distancia < b.distancia
-//
-//                }
-//                    self.show(vc, sender: nil)
-//            }
-//        }
-//    }
-
-
 import UIKit
 
 class EnableSearchViewController: UIViewController {
