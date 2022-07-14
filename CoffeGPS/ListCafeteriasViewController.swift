1cf409f4f04c43177bcf33a3aca522e6f3f3ffba
Franzua <framirzu@emeal.nttdata.com>
2022-07-13 20:18:21 -0500

    sin issues

--- CoffeGPS/ListCafeteriasViewController.swift
+++ CoffeGPS/ListCafeteriasViewController.swift
@@ -19,7 +19,7 @@ class ListCafeteriasViewController: UIViewController {
         tableView.delegate=self
         tableView.dataSource = self
         
-        // Do any additional setup after loading the view.
+        // Hace algo adicional del setup despues de cargar la vista
     }
 
 }
@@ -29,32 +29,34 @@ extension ListCafeteriasViewController: UITableViewDataSource, UITableViewDelega
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int)->
     String?{
         
-        cafeterias.first?.nombre
+        cafeterias.first?.nombre // Pone el nombre del primer local en la lista como cabecera
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cafeterias.count
+        return cafeterias.count // indica el numero de filas que habra en la sección
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "customTableViewCell", for: indexPath) as? CustomTableViewCell
         
-    
-        let cafe = cafeterias[indexPath.row]
+        // Obtiene los datos de cada elemento, es decir obtiene los datos de cada cafeteria por celda
+        
+        let dataDeLaCafeteria = cafeterias[indexPath.row]
         print("cafeterias \(cafeterias)")
         
+    
+        cell?.nameLocalCell?.text = dataDeLaCafeteria.nombre.capitalized
+        cell?.descriptioncell?.text = "Distancia " + String(format : "%0.2f", dataDeLaCafeteria.distancia) + " km"
+        cell?.estrellasCell?.text = "Valoracion: " + String(dataDeLaCafeteria.estrellas) + "⭐️"
+        cell?.direccionesCell?.text = "📌:" + dataDeLaCafeteria.direccion
         
-        cell?.nameLocalCell?.text = cafe.nombre.capitalized
-        cell?.descriptioncell?.text = "Distancia " + String(format : "%0.2f", cafe.distancia) + " km"
-        cell?.estrellasCell?.text = "Valoracion: " + String(cafe.estrellas) + "⭐️"
-        cell?.direccionesCell?.text = "📌:" + cafe.direccion
+        // Obtenemos el url de cada cafeteria y los muestra a traves de UIImage
         
-        
-        if let url = URL(string: cafe.url) {
+        if let url = URL(string: dataDeLaCafeteria.url) {
             let task = URLSession.shared.dataTask(with: url) { data, response, error in guard let data = data, error == nil else { return }
                 
-                DispatchQueue.main.async { /// execute on main threadself.imageView.image = UIImage(data: da
+                DispatchQueue.main.async { /// execute on main threadself.imageView.image = UIImage(data: data)
                     cell?.imageCell?.image = UIImage(data: data)
                 }
             }
@@ -64,13 +66,12 @@ extension ListCafeteriasViewController: UITableViewDataSource, UITableViewDelega
     
         return cell!
     }
-//    
+    //Le dice al Delegate que fila ha sido seleccionada.
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        //tableView.deselectRow(at: indexPath, animated: true)
         performSegue(withIdentifier: "showmecoordenates", sender: self)
     }
-    
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let destination = segue.destination as? MapViewController {
             destination.locales = cafeterias[(tableView.indexPathForSelectedRow?.row)!]
